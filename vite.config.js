9025e9c5e38f25426279727aec7951412c6b056f
rk1710 <131847003+rk1710@users.noreply.github.com>
2025-03-20 00:37:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+timport { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+export const API_URL = 'http://34.237.104.18:4000'
